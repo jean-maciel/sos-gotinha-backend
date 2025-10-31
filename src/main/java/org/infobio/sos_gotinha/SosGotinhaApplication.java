8e4fb2cbb261641094672f1be37afa62a36db88c
Jean Reis <jeanlucas.reis@gmail.com>
2025-10-30 21:12:08 -0300

    first commit

---
+++ src/main/java/org/infobio/sos_gotinha/SosGotinhaApplication.java
@@ -0,0 +1,13 @@
+package org.infobio.sos_gotinha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SosGotinhaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SosGotinhaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/infobio/sos_gotinha/SosGotinhaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.infobio.sos_gotinha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SosGotinhaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
